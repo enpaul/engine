a081fde7b001c876a29562ae3b6842772ccb1e08
Ethan Paul <24588726+enpaul@users.noreply.github.com>
2023-08-16 01:30:55 -0400

    Add python package scaffolding

---
+++ engine/__about__.py
@@ -0,0 +1,7 @@
+# pylint: disable=missing-docstring
+__title__ = "engine"
+__summary__ = "A collaborative file server for engineers"
+__version__ = "0.0.0"
+__url__ = "https://github.com/enpaul/engine/"
+__license__ = "GPL3"
+__authors__ = ["Ethan Paul <24588726+enpaul@users.noreply.github.com>"]
